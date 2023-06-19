f0f5f3fafdb58f5d8b415075720f56f9dd5e1866
Diego Alonso Rubí Salas <diegoalru@gmail.com>
2023-06-18 18:36:46 -0600

    Configurar el actionBar.

--- app/src/main/java/com/darssolutions/cupcake/MainActivity.kt
+++ app/src/main/java/com/darssolutions/cupcake/MainActivity.kt
@@ -1,5 +1,18 @@
 package com.darssolutions.cupcake
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
-class MainActivity: AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Set up the action bar for use with the NavController
+        val navHostfragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostfragment.navController
+        setupActionBarWithNavController(navController)
+    }
+}
