e9f51a5b669226292516eb5659c43c92aab9169b
Diego Alonso Rubí Salas <diegoalru@gmail.com>
2023-06-19 13:06:39 -0600

    Creación de lista de fechas de retiro.

--- app/src/main/java/com/darssolutions/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/darssolutions/cupcake/model/OrderViewModel.kt
@@ -3,6 +3,9 @@ package com.darssolutions.cupcake.model
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 /**
  * ViewModel containing all the order details, including the quantity, flavor, date, and price.
@@ -11,6 +14,7 @@ import androidx.lifecycle.ViewModel
  */
 class OrderViewModel : ViewModel() {
 
+    // region Properties
     private var _quantity = MutableLiveData<Int>(0)
     val quantity: LiveData<Int> = _quantity
 
@@ -23,6 +27,10 @@ class OrderViewModel : ViewModel() {
     private var _price = MutableLiveData<Double>(0.0)
     val price: LiveData<Double> = _price
 
+    val dateOptions = getPickupOptions()
+// endregion
+
+    // region Methods
     fun setQuantity(numOfCakes: Int) {
         _quantity.value = numOfCakes
     }
@@ -38,4 +46,19 @@ class OrderViewModel : ViewModel() {
     fun hasNoFlavorSet(): Boolean {
         return _flavor.value.isNullOrEmpty()
     }
+
+    fun getPickupOptions(): List<String> {
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+
+        // Create a list of dates starting with the current date and the following 3 days
+        repeat(4) {
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+
+        return options
+    }
+// endregion
 }
