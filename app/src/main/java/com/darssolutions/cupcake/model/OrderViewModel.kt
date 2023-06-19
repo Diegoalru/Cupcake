8b53341f972d8811af7ee136138b72c7d3c713e7
Diego Alonso Rubí Salas <diegoalru@gmail.com>
2023-06-19 12:21:29 -0600

    ViewModel para la aplicación.

---
+++ app/src/main/java/com/darssolutions/cupcake/model/OrderViewModel.kt
@@ -0,0 +1,37 @@
+package com.darssolutions.cupcake.model
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+/**
+ * ViewModel containing all the order details, including the quantity, flavor, date, and price.
+ * This class is used to share data between the screens of the app.
+ * The ViewModel is automatically retained during configuration changes by the architecture.
+ */
+class OrderViewModel : ViewModel() {
+
+    private var _quantity = MutableLiveData<Int>(0)
+    val quantity: LiveData<Int> = _quantity
+
+    private var _flavor = MutableLiveData<String>("")
+    val flavor: LiveData<String> = _flavor
+
+    private var _date = MutableLiveData<String>("")
+    val date: LiveData<String> = _date
+
+    private var _price = MutableLiveData<Double>(0.0)
+    val price: LiveData<Double> = _price
+
+    fun setQuantity(numOfCakes: Int) {
+        _quantity.value = numOfCakes
+    }
+
+    fun setFlavor(desiredFlavor: String) {
+        _flavor.value = desiredFlavor
+    }
+
+    fun setDate(date: String) {
+        _date.value = date
+    }
+}
