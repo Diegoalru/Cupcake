f6dbd09b8069eca4918c5d2a49683b02c8de2377
Diego Alonso Rubí Salas <diegoalru@gmail.com>
2023-06-19 22:08:07 -0600

    Habilitar el comportamiento de ButtonUp.

--- app/src/main/java/com/darssolutions/cupcake/MainActivity.kt
+++ app/src/main/java/com/darssolutions/cupcake/MainActivity.kt
@@ -2,17 +2,26 @@ package com.darssolutions.cupcake
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Set up the action bar for use with the NavController
-        val navHostfragment = supportFragmentManager
+        val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostfragment.navController
+        navController = navHostFragment.navController
         setupActionBarWithNavController(navController)
+    }
+
+    // Handle navigation when the user presses the back button
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
